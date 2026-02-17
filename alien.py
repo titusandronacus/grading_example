39e71e9d346033fa79ae934232f68b605153eda7
Travis Burke <tburke22@cscc.edu>
2026-02-17 09:45:38 -0500

    added red and yellow checks

--- alien.py
+++ alien.py
@@ -6,6 +6,8 @@ alien_shot = input("What color was the alien? ")
 
 if alien_shot == "green":
     print("You scored 5 points")
+elif alien_shot == "yellow":
+    print("You socred 10 points")
 else:
-    print("You scored 10 points")
+    print("You scored 15 points")
 
